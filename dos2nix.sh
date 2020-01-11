b21577d89974e48de0ecd17a705b6152f6c2cad5
Kelves <kelves_16@hotmail.com>
2020-01-11 13:43:51 -0300

    Reformatted text.

--- dos2nix.sh
+++ dos2nix.sh
@@ -4,14 +4,18 @@
 
 # Usage: dos2nix.sh < input_file > output_file
 
+#  to display these Ctrled characters on text
+
+#sed 's/$//'
+#sed 's///g'
+
+# Whe we don't want to delete '\n'
+#sed ':a;N;$!ba;s/\n//g'
+
+# This one below just tries to delete the CR
+#sed -e 's/.$//'
+
 #TODO: Imporve this piece of shit
 tr -d '\r'
 #tr -d '\015'
 
-#  to display these Ctrled characters on text
-#sed 's/$//'
-#sed -e 's///g'
-# Whe we don't want to delete '\n'
-#sed ':a;N;$!ba;s/\n//g'
-# This one below just tries to delete the CR
-#sed -e 's/.$//'
