8e60221e005986a63c3dbbf03fec01f082e8f4bc
Kelves <kelves_16@hotmail.com>
2020-04-12 23:32:52 -0300

    initial commit for usblistener.sh, script that receive notification from a fifo.

---
+++ usblistener.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+
+trap 'rm "$PIPE"' 10
+
+PIPE="/tmp/PIPE"
+
+check_if_fifo_exists ()
+{
+    [ -p "$PIPE" ] || mkfifo "$PIPE"
+}
+
+listen_to_fifo ()
+{
+    while true;
+    do
+        if check_if_fifo_exists;
+        then
+            SIGNAL=$(cat "$PIPE")
+            dunstify "$SIGNAL"
+        fi
+        sleep .5
+    done
+}
+
+listen_to_fifo
+
