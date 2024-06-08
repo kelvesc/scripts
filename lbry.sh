842b90d0165278fa58d8f38f479a42ebf53460af
Kelves <kelves.nunes@ufrpe.br>
2024-06-08 17:16:54 -0300

    feat: added sanity checks and a function to address errors during usage.

--- lbry.sh
+++ lbry.sh
@@ -1,32 +1,68 @@
 #!/bin/bash
 
 # Author: Kelves C.
-# Version: 0.6.0-beta (usable)
+# Version: 1.0.0
 # License: Public Domain
-# Description:
-# Search through the subtree of a directory where pdfs are stored
-# and open the selected file in zathura
 
-LIBRARIES=($HOME/papers_lib)
+set -e
 
-while true;
-do
-    case $1 in
-        cli)    PAPER=$(find ${LIBRARIES[@]} -type f -name *.pdf | fzy) ;;
-        gui)    PAPER=$(find ${LIBRARIES[@]} -type f -name *.pdf | \
-                        rofi -lines 15 -dmenu -p "read") ;;
-    esac
-    if [ -f "$PAPER" ];
-    then
-        # Initializes the pdfreader program
-  	    zathura "$PAPER" &
-        break
-    elif [ ! -e "$PAPER" ];
-    then
-  	    exit 1
-    else
-    targetDir="$PAPER"
-    fi
-done
+ARGS="$@"
 
-exit 0
+fail(){
+        echo "ERROR: $@"
+        exit 1
+}
+
+if ! command -v fzy; then
+        fail "fzy not installed."
+fi
+
+if ! command -v rofi; then
+        fail "rofi not installed."
+fi
+
+if ! command -v zathura; then
+        fail "zathura not installed."
+fi
+
+readonly DEPENDENCIES="fzy rofi zathura"
+_check_dependencies(){
+        for dep in $DEPENDENCIES; do
+                if ! command -v $dep; then
+                        fail "$dep not installed."
+                fi
+        done
+}
+
+PDF_DIR=${2:"${HOME}/papers"}
+if [[ -z $PDF_DIR ]]; then
+        if [[ -d "${PDF_DIR}" ]]; then
+                fail "directory ${PDF_DIR} does not exists."
+        fi
+fi
+
+run(){
+        case $1 in
+                cli)    readonly menu=fzy ;;
+                gui)    readonly menu="rofi -lines 15 -dmenu -p 'read' " ;;
+        esac
+
+        while true; do
+
+                PDF_DIR=$(find ${PDF_DIR[@]} -type f -name *.pdf | "${menu}")
+
+                if [ ! -e "$PDF_DIR" ]; then
+                        fail "${PDF_DIR} does not exists."
+                fi
+
+                if [ -f "$PDF_DIR" ]; then
+                        zathura "$PDF_DIR" &
+                        break
+                fi
+
+        done
+
+        exit 0
+}
+
+run "${ARGS}"
